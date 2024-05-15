192635b48a6968ffdc2fd0a0794afa5cf69da836
Charlotte Evans <charlotteevans543@gmail.com>
2024-05-15 13:59:52 +0200

    Created input form for users to add their name and question to the magic-ball. Created a <p> tag to use/learn string interpolation. Changed from else if statements to switch/case. Commented out ternary operator that checks whether there is a name.

--- src/script.js
+++ src/script.js
@@ -1,31 +1,60 @@
-let userName = 'Jane'
+let userName = '';
 
-userName ? console.log(`Hello, ${userName}`)
-    : console.log('Hello!')
+document.getElementById('eightBallForm').onsubmit = function(event) {
+    event.preventDefault()
+    askEightBall()
+};
 
-let userQuestion = `${userName} took the words right out of my mouth.`
-console.log(userQuestion)
+function askEightBall() {
+    let userQuestion = document.getElementById('userQuestion').value
+    userName = document.getElementById('userNameID').value; // Removed let to use the global userName variable
 
-const randomNumber = Math.floor(Math.random() * 8)
-let eightBall = randomNumber
+    // .trim()checks to see whether there are any whitespaces // Checks to see if the user has entered a question and their name
+    if (userQuestion.trim() === '' || userQuestion === '') {
+        alert('Please write a question and your name to ask the Magic 8-Ball!')
+        return; // Added return to stop the function if no question is asked
+    }
 
-if (randomNumber === 0) {
-    console.log('It is certain')
-} else if (randomNumber === 1) {
-    console.log('It is decidedly so')
-} else if (randomNumber === 2) {
-    console.log('Reply hazy try again')
-} else if (randomNumber === 3) {
-    console.log('Cannot predict now')
-} else if (randomNumber === 4) {
-    console.log('Do not count on it')
-} else if (randomNumber === 5) {
-    console.log('My sources say no')
-} else if (randomNumber === 6) {
-    console.log('Outlook not so good')
-} else if (randomNumber === 7) {
-    console.log('Signs point to yes')
+     let ballComment = document.getElementById('ballComment').innerText = `Let's see what the magic-ball has to say to ${userName}'s question..`; // Added a comment to the user
+
+
+    // Added a random number generator to determine the eight-ball message
+    const randomNumber = Math.floor(Math.random() * 8);
+    // Provided a variable to store the eight-ball message
+    let eightBallMessage = ''
+
+    // Switch statement to determine the eight-ball message
+    switch (randomNumber) {
+        case 0:
+            eightBallMessage = 'It is certain'
+            break;
+        case 1:
+            eightBallMessage = 'It is decidedly so'
+            break;
+        case 2:
+            eightBallMessage = 'Reply hazy try again'
+            break;
+        case 3:
+            eightBallMessage = 'Cannot predict now'
+            break;
+        case 4:
+            eightBallMessage = 'Do not count on it'
+            break;
+        case 5:
+            eightBallMessage = 'My sources say no'
+            break;
+        case 6:
+            eightBallMessage = 'Outlook not so good'
+            break;
+        case 7:
+            eightBallMessage = 'Signs point to yes'
+            break;
+    }
+
+    console.log(eightBallMessage); // Log the message
+    document.getElementById('eightBallOutput').innerText = eightBallMessage; // Display the message to the user
 }
 
-console.log(eightBall)
-document.getElementById('eightBallOutput').innerText = eightBall;
+
+/* userName ? console.log(`Hello, ${userName}`) : console.log('Hello!');
+    console.log(`${userName} asks: ${userQuestion}`)*/
